882eb2f0798bebdcc730372e5fc4a1a774b54dd4
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-10-08 16:26:15 +0200

    Formatting and improving test

--- src/test/kotlin/no/nav/soknad/innsending/service/SoknadServiceTest.kt
+++ src/test/kotlin/no/nav/soknad/innsending/service/SoknadServiceTest.kt
@@ -8,10 +8,9 @@ import no.nav.soknad.innsending.repository.SoknadRepository
 import no.nav.soknad.innsending.repository.SoknadsStatus
 import no.nav.soknad.innsending.repository.VedleggRepository
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.io.ByteArrayOutputStream
@@ -19,175 +18,175 @@ import java.time.LocalDateTime
 import java.util.*
 
 @SpringBootTest
-internal class SoknadServiceTest {
+class SoknadServiceTest {
 
-    @Autowired
-    private lateinit var soknadRepository: SoknadRepository
+	@Autowired
+	private lateinit var soknadRepository: SoknadRepository
 
-    @Autowired
-    private lateinit var vedleggRepository: VedleggRepository
+	@Autowired
+	private lateinit var vedleggRepository: VedleggRepository
 
-    @Autowired
-    private lateinit var skjemaService: HentSkjemaDataConsumer
+	@Autowired
+	private lateinit var skjemaService: HentSkjemaDataConsumer
 
-    @AfterEach
-    fun ryddOpp() {
-        vedleggRepository.deleteAll()
-        soknadRepository.deleteAll()
-    }
+	@AfterEach
+	fun ryddOpp() {
+		vedleggRepository.deleteAll()
+		soknadRepository.deleteAll()
+	}
 
-    @Test
-    fun opprettSoknadGittSkjemanr() {
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
+	@Test
+	fun opprettSoknadGittSkjemanr() {
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
 
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
 
-        assertTrue(dokumentSoknadDto != null)
+		assertEquals(brukerid, dokumentSoknadDto.brukerId)
+		assertEquals(skjemanr, dokumentSoknadDto.skjemanr)
+		assertEquals(spraak, dokumentSoknadDto.spraak)
+	}
 
-    }
+	@Test
+	fun opprettSoknadGittSoknadDokument() {
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val tittel = "Tittel"
+		val tema = "tema"
+		val dokumentSoknadDto = lagDokumentSoknad(brukerid, skjemanr, spraak, tittel, tema)
 
-    @Test
-    fun opprettSoknadGittSoknadDokument() {
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val tittel = "Tittel"
-        val tema = "tema"
-        val dokumentSoknadDto = lagDokumentSoknad(brukerid, skjemanr, spraak, tittel, tema)
+		val lagretDokumentSoknadDto = soknadService.opprettEllerOppdaterSoknad(dokumentSoknadDto)
 
-        val lagretDokumentSoknadDto = soknadService.opprettEllerOppdaterSoknad(dokumentSoknadDto)
+		assertNotNull(lagretDokumentSoknadDto.id)
+		assertNotNull(lagretDokumentSoknadDto.behandlingsId)
+		assertEquals(brukerid, lagretDokumentSoknadDto.brukerId)
+		assertTrue(lagretDokumentSoknadDto.vedleggsListe.isNotEmpty())
+	}
 
-        assertTrue(lagretDokumentSoknadDto != null)
-        assertTrue(lagretDokumentSoknadDto.id != null)
-        assertTrue(lagretDokumentSoknadDto.behandlingsId != null)
-        assertTrue(lagretDokumentSoknadDto.brukerId.equals(brukerid))
-        assertTrue(!lagretDokumentSoknadDto.vedleggsListe.isNullOrEmpty() && lagretDokumentSoknadDto.vedleggsListe.stream().findFirst().isPresent)
+	@Test
+	fun hentOpprettetSoknadDokument() {
 
-    }
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
 
-    @Test
-    fun hentOpprettetSoknadDokument() {
+		val dokumentSoknadDtoHentet = soknadService.hentSoknad(dokumentSoknadDto.id!!)
 
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		assertEquals(dokumentSoknadDto.id, dokumentSoknadDtoHentet.id)
+	}
 
-        val dokumentSoknadDtoHentet = soknadService.hentSoknad(dokumentSoknadDto.id!!)
+	@Test
+	fun hentOpprettetVedlegg() {
 
-        assertTrue(dokumentSoknadDto.id == dokumentSoknadDtoHentet.id)
-    }
-    @Test
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
 
-    fun hentOpprettetVedlegg() {
+		assertFalse(dokumentSoknadDto.vedleggsListe.isEmpty())
 
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		val vedleggDto = soknadService.hentVedlegg(dokumentSoknadDto.vedleggsListe[0].id!!)
 
-        assertTrue(dokumentSoknadDto != null && !dokumentSoknadDto.vedleggsListe.isNullOrEmpty())
+		assertEquals(vedleggDto.id, dokumentSoknadDto.vedleggsListe[0].id!!)
+	}
 
-        val vedleggDto = soknadService.hentVedlegg(dokumentSoknadDto.vedleggsListe.get(0).id!!)
+	@Test
+	fun slettOpprettetSoknadDokument() {
 
-        assertTrue(vedleggDto != null && vedleggDto.id == dokumentSoknadDto.vedleggsListe.get(0).id!!)
-    }
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		soknadService.slettSoknad(dokumentSoknadDto.id!!)
 
-    @Test
-    fun slettOpprettetSoknadDokument() {
+		assertThrows<Exception> {
+			soknadService.hentSoknad(dokumentSoknadDto.id!!)
+		}
+	}
 
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
-        soknadService.slettSoknad(dokumentSoknadDto.id!!)
+	@Test
+	fun oppdaterVedlegg() {
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
 
-        org.junit.jupiter.api.assertThrows<Exception> {
-            soknadService.hentSoknad(dokumentSoknadDto.id!!)
-        }
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
 
-    }
+		assertEquals(brukerid, dokumentSoknadDto.brukerId)
+		assertEquals(skjemanr, dokumentSoknadDto.skjemanr)
+		assertEquals(spraak, dokumentSoknadDto.spraak)
 
-    @Test
-    fun oppdaterVedlegg() {
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
+		val oppdatertVedleggDto = lastOppDokumentTilVedlegg(dokumentSoknadDto.vedleggsListe[0])
 
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		val oppdatertVedleggDtoDb = soknadService.lagreVedlegg(oppdatertVedleggDto, dokumentSoknadDto.id!!)
 
-        assertTrue(dokumentSoknadDto != null)
+		assertEquals(oppdatertVedleggDtoDb.id, oppdatertVedleggDto.id)
+		assertEquals(oppdatertVedleggDtoDb.erHoveddokument, oppdatertVedleggDtoDb.erHoveddokument)
+	}
 
-        val oppdatertVedleggDto = lastOppDokumentTilVedlegg(dokumentSoknadDto.vedleggsListe.get(0))
+	@Test
+	fun oppdaterSoknadOgVedlegg() {
+		val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository)
 
-        val oppdatertVedleggDtoDb = soknadService.lagreVedlegg(oppdatertVedleggDto, dokumentSoknadDto.id!!)
+		val brukerid = "12345678901"
+		val skjemanr = "NAV 95-00.11"
+		val spraak = "no"
+		val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
 
-        assertTrue(oppdatertVedleggDtoDb != null)
-        assertTrue(oppdatertVedleggDtoDb.id == oppdatertVedleggDto.id)
-        assertTrue(oppdatertVedleggDtoDb.erHoveddokument == oppdatertVedleggDtoDb.erHoveddokument)
+		assertEquals(brukerid, dokumentSoknadDto.brukerId)
+		assertEquals(skjemanr, dokumentSoknadDto.skjemanr)
+		assertEquals(spraak, dokumentSoknadDto.spraak)
 
-    }
+		val dokumentSoknadDtoOppdatert = oppdaterDokumentSoknad(dokumentSoknadDto)
 
-    @Test
-    fun oppdaterSoknadOgVedlegg() {
-        val soknadService = SoknadService(skjemaService, soknadRepository, vedleggRepository )
+		val dokumentSoknadDtoOppdatertDb = soknadService.opprettEllerOppdaterSoknad(dokumentSoknadDtoOppdatert)
 
-        val brukerid = "12345678901"
-        val skjemanr = "NAV 95-00.11"
-        val spraak = "no"
-        val dokumentSoknadDto = soknadService.opprettSoknad(brukerid, skjemanr, spraak)
+		assertEquals(dokumentSoknadDtoOppdatertDb.id, dokumentSoknadDtoOppdatert.id)
+		assertEquals(dokumentSoknadDtoOppdatertDb.vedleggsListe[0].id, dokumentSoknadDtoOppdatert.vedleggsListe[0].id)
+		assertEquals(dokumentSoknadDtoOppdatertDb.vedleggsListe[0].erHoveddokument, dokumentSoknadDtoOppdatert.vedleggsListe[0].erHoveddokument)
+	}
 
-        assertTrue(dokumentSoknadDto != null)
+	private fun lagDokumentSoknad(brukerId: String, skjemanr: String, spraak: String, tittel: String, tema: String): DokumentSoknadDto {
+			val vedleggDtoPdf = VedleggDto(null, skjemanr, tittel, null, UUID.randomUUID().toString(), "application/pdf",
+					getBytesFromFile("/litenPdf.pdf"), true, erVariant = false, true, OpplastingsStatus.LastetOpp,  LocalDateTime.now())
+			val vedleggDtoJson = VedleggDto(null, skjemanr, tittel, null, UUID.randomUUID().toString(),"application/json",
+					getBytesFromFile("/sanity.json"), true, erVariant = true, false, OpplastingsStatus.LastetOpp, LocalDateTime.now())
 
-        val dokumentSoknadDtoOppdatert = oppdaterDokumentSoknad(dokumentSoknadDto)
+		val vedleggDtoList = listOf(vedleggDtoPdf, vedleggDtoJson)
+		return DokumentSoknadDto(null, null, null, brukerId, skjemanr, tittel, tema, spraak,
+			SoknadsStatus.Opprettet, LocalDateTime.now(), LocalDateTime.now(), null, vedleggDtoList)
+	}
 
-        val dokumentSoknadDtoOppdatertDb = soknadService.opprettEllerOppdaterSoknad(dokumentSoknadDtoOppdatert)
+	private fun oppdaterDokumentSoknad(dokumentSoknadDto: DokumentSoknadDto): DokumentSoknadDto {
+		val vedleggDto = lastOppDokumentTilVedlegg(dokumentSoknadDto.vedleggsListe[0])
+		return DokumentSoknadDto(dokumentSoknadDto.id, dokumentSoknadDto.behandlingsId, dokumentSoknadDto.ettersendingsId,
+			dokumentSoknadDto.brukerId, dokumentSoknadDto.skjemanr, dokumentSoknadDto.tittel, dokumentSoknadDto.tema,
+			dokumentSoknadDto.spraak, SoknadsStatus.Opprettet, dokumentSoknadDto.opprettetDato, LocalDateTime.now(),
+			null, listOf(vedleggDto))
+	}
 
-        assertTrue(dokumentSoknadDtoOppdatertDb != null)
-        assertTrue(dokumentSoknadDtoOppdatertDb.id == dokumentSoknadDtoOppdatert.id)
-        assertTrue(dokumentSoknadDtoOppdatertDb.vedleggsListe.get(0).id == dokumentSoknadDtoOppdatert.vedleggsListe.get(0).id)
-        assertTrue(dokumentSoknadDtoOppdatertDb.vedleggsListe.get(0).erHoveddokument == dokumentSoknadDtoOppdatert.vedleggsListe.get(0).erHoveddokument)
+	private fun lastOppDokumentTilVedlegg(vedleggDto: VedleggDto) =
+		VedleggDto(vedleggDto.id, vedleggDto.vedleggsnr, vedleggDto.tittel, vedleggDto.skjemaurl, UUID.randomUUID().toString(),
+			"application/pdf", getBytesFromFile("/litenPdf.pdf"), true, erVariant = false,
+			true, OpplastingsStatus.LastetOpp, LocalDateTime.now())
 
-    }
-
-    fun lagDokumentSoknad(brukerId: String, skjemanr: String, spraak: String, tittel: String, tema: String): DokumentSoknadDto {
-        val vedleggDtoPdf = VedleggDto(null, skjemanr, tittel, null, UUID.randomUUID().toString(), "application/pdf"
-            ,getBytesFromFile("/litenPdf.pdf"), true, false, true, OpplastingsStatus.LastetOpp,  LocalDateTime.now())
-        val vedleggDtoJson = VedleggDto(null, skjemanr, tittel, null, UUID.randomUUID().toString(),"application/json"
-            , getBytesFromFile("/sanity.json"), true, erVariant = true, false, OpplastingsStatus.LastetOpp, LocalDateTime.now())
-
-        val vedleggDtoList = listOf(vedleggDtoPdf, vedleggDtoJson)
-        return DokumentSoknadDto(null, null, null, brukerId, skjemanr, tittel, tema, spraak, SoknadsStatus.Opprettet, LocalDateTime.now(), LocalDateTime.now(), null, vedleggDtoList )
-    }
-
-    fun oppdaterDokumentSoknad(dokumentSoknadDto: DokumentSoknadDto): DokumentSoknadDto {
-        val vedleggDto = lastOppDokumentTilVedlegg(dokumentSoknadDto.vedleggsListe.get(0))
-        return DokumentSoknadDto(dokumentSoknadDto.id, dokumentSoknadDto.behandlingsId, dokumentSoknadDto.ettersendingsId, dokumentSoknadDto.brukerId, dokumentSoknadDto.skjemanr, dokumentSoknadDto.tittel, dokumentSoknadDto.tema
-            , dokumentSoknadDto.spraak, SoknadsStatus.Opprettet, dokumentSoknadDto.opprettetDato, LocalDateTime.now(), null, listOf(vedleggDto) )
-    }
-
-    fun lastOppDokumentTilVedlegg(vedleggDto: VedleggDto): VedleggDto {
-        return VedleggDto(vedleggDto.id, vedleggDto.vedleggsnr, vedleggDto.tittel, vedleggDto.skjemaurl, UUID.randomUUID().toString(), "application/pdf"
-            ,getBytesFromFile("/litenPdf.pdf"), true, false, true, OpplastingsStatus.LastetOpp,  LocalDateTime.now())
-    }
-
-    fun getBytesFromFile(path: String): ByteArray {
-        val resourceAsStream = SoknadServiceTest::class.java.getResourceAsStream(path)
-        val outputStream = ByteArrayOutputStream()
-        resourceAsStream.use { input ->
-            outputStream.use { output ->
-                input!!.copyTo(output)
-            }
-        }
-        return outputStream.toByteArray()
-    }
-
+	private fun getBytesFromFile(path: String): ByteArray {
+		val resourceAsStream = SoknadServiceTest::class.java.getResourceAsStream(path)
+		val outputStream = ByteArrayOutputStream()
+		resourceAsStream.use { input ->
+			outputStream.use { output ->
+				input!!.copyTo(output)
+			}
+		}
+		return outputStream.toByteArray()
+	}
 }
