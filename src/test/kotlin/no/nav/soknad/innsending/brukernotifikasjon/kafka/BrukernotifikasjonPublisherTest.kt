8fb9e1f88eb39b02f63563dbaec26af347f859f1
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-10-20 10:56:32 +0200

    SD-1543-Initiell versjon med mange mangler. Lagt til spesifisering av repositories for å kunne finne Brukernotifikasjons spesifikkasjoner

--- src/test/kotlin/no/nav/soknad/innsending/brukernotifikasjon/kafka/BrukernotifikasjonPublisherTest.kt
+++ src/test/kotlin/no/nav/soknad/innsending/brukernotifikasjon/kafka/BrukernotifikasjonPublisherTest.kt
@@ -1,12 +1,44 @@
 package no.nav.soknad.innsending.brukernotifikasjon.kafka
 
+import no.nav.brukernotifikasjon.schemas.Beskjed
+import no.nav.soknad.innsending.repository.OpplastingsStatus
+import org.joda.time.DateTime
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class BrukernotifikasjonPublisherTest {
 
-    @Test
-    fun soknadStatusChange() {
-    }
+
+/*
+
+	@Test
+	fun `sjekk melding om ny dokumentinnsending blir publisert`() {
+
+		val henvendelsesId = "123456"
+		val forrigeHenvendelse = null
+		val ettersendelse = false
+		val dokumentinnsending = true
+		val personId = "12125912345"
+		val tema = "PEN"
+		val endringsDato = DateTime.now()
+		val status = OpplastingsStatus.LASTET_OPP
+		val tittel = "Dokumentasjon av utdanning"
+		val xml = lagMetadataliste(false, "{\"tittel\":\"$tittel\",\"tema\":\"$tema\"}")
+
+		val message = slot<Beskjed>()
+		every { kafkaPublisher.putApplicationMessageOnTopic(any(), capture(message)) } returns Unit
+
+		soknadStatusChangePublisher.soknadStatusChange(henvendelsesId, forrigeHenvendelse, ettersendelse, dokumentinnsending, personId, tema, endringsDato, status, marshal(xml))
+
+		assertTrue(message.isCaptured)
+		assertEquals(personId, message.captured.getFodselsnummer())
+		assertEquals(henvendelsesId, message.captured.getGrupperingsId())
+		assertTrue(message.captured.getTekst().contains(tittel))
+		assertEquals(soknadStatusChangePublisher.tittelPrefixNySoknad + tittel, message.captured.getTekst())
+		assertEquals("https://localhost" + soknadStatusChangePublisher.linkDokumentinnsending + henvendelsesId, message.captured.getLink())
+
+	}
+*/
+
 }
