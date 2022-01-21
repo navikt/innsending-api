5d4474cf1329b98a5e56963f0f348bad244e14d3
nils-arne <50109859+nils-arne@users.noreply.github.com>
2022-01-21 09:45:00 +0100

    Lagt til logging ved henting av skjema fra Sanity

--- src/main/kotlin/no/nav/soknad/innsending/consumerapis/skjema/SkjemaClient.kt
+++ src/main/kotlin/no/nav/soknad/innsending/consumerapis/skjema/SkjemaClient.kt
@@ -1,6 +1,7 @@
 package no.nav.soknad.innsending.consumerapis.skjema
 
 import no.nav.soknad.innsending.config.RestConfig
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.beans.factory.annotation.Qualifier
@@ -11,7 +12,10 @@ class SkjemaClient(private val restConfig: RestConfig,
 									 @Qualifier("basicClient") private val webClient: WebClient
 ) {
 
+	private val logger = LoggerFactory.getLogger(javaClass)
+
 	fun hent(): List<SkjemaOgVedleggsdata>? {
+			logger.info("Hent skjema fra Sanity ${restConfig.sanityHost+restConfig.sanityEndpoint}")
 			val skjemaer = webClient
 				.get()
 				.uri(restConfig.sanityHost+restConfig.sanityEndpoint)
@@ -28,7 +32,9 @@ class SkjemaClient(private val restConfig: RestConfig,
 				.collectList()
 				.block()
 
-			if (skjemaer != null && skjemaer.get(0) != null && skjemaer.get(0).skjemaer!!.isNotEmpty()) {
+		logger.info("Skjema hentet fra Sanity ${skjemaer}")
+
+		if (skjemaer != null && skjemaer.get(0) != null && skjemaer.get(0).skjemaer!!.isNotEmpty()) {
 				return skjemaer.get(0)?.skjemaer ?: emptyList()
 			}
 			throw RuntimeException("Feil ved forsøk på henting av skjema fra sanity")
