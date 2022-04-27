fbb4b8b32ef86c017c7e43c834ab03078150113c
nils-arne <50109859+nils-arne@users.noreply.github.com>
2022-04-27 15:07:55 +0200

    Eksperimentering med cors header.

--- innsender/src/main/kotlin/no/nav/soknad/innsending/config/WebSecurityConfig.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/config/WebSecurityConfig.kt
@@ -2,13 +2,19 @@ package no.nav.soknad.innsending.config
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.List
+
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -17,9 +23,17 @@ class WebSecurityConfig(private val config: RestConfig) : WebSecurityConfigurerA
 	private val logger = LoggerFactory.getLogger(javaClass)
 
 	override fun configure(http: HttpSecurity) {
+		val corsConfiguration = CorsConfiguration()
+		corsConfiguration.allowedHeaders = List.of("Authorization", "Cache-Control", "Content-Type")
+		corsConfiguration.allowedOrigins = List.of("*")
+		corsConfiguration.allowedMethods =
+			List.of(RequestMethod.GET.name, RequestMethod.POST.name, RequestMethod.DELETE.name, RequestMethod.OPTIONS.name, RequestMethod.PATCH.name)
+		corsConfiguration.allowCredentials = true
+		corsConfiguration.exposedHeaders = List.of("Authorization")
+
 		http
 			.csrf().disable()
-			.cors()
+			.cors().configurationSource { request -> corsConfiguration }
 			.and()
 			.authorizeRequests()
 			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
