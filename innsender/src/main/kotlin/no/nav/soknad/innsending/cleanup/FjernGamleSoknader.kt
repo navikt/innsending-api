9a730681e174591137e5f769f4191b74b70b5880
nils-arne <50109859+nils-arne@users.noreply.github.com>
2022-05-10 09:25:46 +0200

    Deploy feilet på linje 86. Endret casing på env properties. Lagt til leaderElection slik at ryddejobb kun kjøres på en node i clusteret.

--- innsender/src/main/kotlin/no/nav/soknad/innsending/cleanup/FjernGamleSoknader.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/cleanup/FjernGamleSoknader.kt
@@ -1,18 +1,52 @@
 package no.nav.soknad.innsending.cleanup
 
 import no.nav.soknad.innsending.service.SoknadService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.net.InetAddress
+import java.net.URL
 
 @Service
 class FjernGamleSoknader(private val soknadService: SoknadService) {
+
+	private val logger = LoggerFactory.getLogger(javaClass)
 
 	@Value("\${cron.slettEldreEnn}")
 	private lateinit var dagerGamleString: String
 
 	@Scheduled(cron = "\${cron.slettGamleIkkeInnsendteSoknader}")
 	fun fjernGamleIkkeInnsendteSoknader() {
-		soknadService.slettGamleIkkeInnsendteSoknader(dagerGamleString.toLong())
+		if (isLeader()) {
+			soknadService.slettGamleIkkeInnsendteSoknader(dagerGamleString.toLong())
+		}
 	}
+
+	private fun isLeader(): Boolean {
+		logger.info("Sjekk om leader")
+		val electorPath = System.getenv("ELECTOR_PATH") ?: System.getProperty("ELECTOR_PATH")
+		if (electorPath.isNullOrBlank()) {
+			logger.info("ELECTOR_PATH er null eller blank")
+			return false
+		}
+		try {
+			logger.info("Elector_path=$electorPath")
+			val fullUrl = if (electorPath.contains(":/")) electorPath else "http://$electorPath"
+			val jsonString = URL(fullUrl).readText()
+			logger.info("Elector_path som jsonstring=$jsonString")
+
+			val leader = JSONObject(jsonString).getString("name")
+			val hostname = InetAddress.getLocalHost().hostName
+
+			logger.info("isLeader=${hostname.equals(leader, true)}")
+			return hostname.equals(leader, true)
+
+		} catch (exception: Exception) {
+			logger.warn("Sjekk om leader feilet med:", exception)
+			return false
+		}
+	}
+
 }
