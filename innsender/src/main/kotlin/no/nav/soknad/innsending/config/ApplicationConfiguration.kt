ea4948068bcf254d3a67819ff0d9c23381199aaf
nils-arne <50109859+nils-arne@users.noreply.github.com>
2022-08-09 18:11:46 +0200

    Ta bort overføldig logging av: "This generated password is for development use only. Your security configuration must be updated before running your application in production."

--- innsender/src/main/kotlin/no/nav/soknad/innsending/config/ApplicationConfiguration.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/config/ApplicationConfiguration.kt
@@ -1,17 +1,20 @@
 package no.nav.soknad.innsending.config
 
-import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.core.Authentication
 
 
-@EnableOAuth2Client(cacheEnabled = true)
 @Configuration
-@Profile("dev | prod")
-class TokenClientConfig
-
-
+class ApplicationConfiguration {
+	@Bean
+	fun noopAuthenticationManager(): AuthenticationManager? {
+		return AuthenticationManager { authentication: Authentication? ->
+			throw AuthenticationServiceException(
+				"Bruker tokenx validering for autentisering"
+			)
+		}
+	}
+}
