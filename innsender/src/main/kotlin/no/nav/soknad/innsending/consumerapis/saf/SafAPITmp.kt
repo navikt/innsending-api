5ddbb62a47697a969f5870aa5102693d4b9e575d
nils-arne <50109859+nils-arne@users.noreply.github.com>
2022-10-21 14:17:24 +0200

    Retting av feil funnet i forbindelse med forsøk på prodsetting. I loggene kommer warning: "Skjema med id = NAVe 54-00.08 ikke funnet".  Dette skjer fordi ved ettersending der søker allerede har sendt inn ettersending på et gitt skjema, feiler oppretting av ny ettersendingssøknad da oppslag mot sanity feiler fordi skjemanr er hentet fra arkivet og inneholder NAVe. Rettet kode slik at alle skjemanr på alle dokumenter hentet fra arkivet som starter med NAVe endres til NAV.

--- innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/saf/SafAPITmp.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/saf/SafAPITmp.kt
@@ -30,28 +30,69 @@ class SafAPITmp: SafInterface, HealthRequestInterface {
 		return dummyArkiverteSoknader[brukerId] ?: emptyList()
 	}
 
-	private val date = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now())
+	private val date = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(0))
 
 	private val dummyArkiverteSoknader = mapOf (
 		testpersonid to listOf (
 			ArkiverteSaker(
-				Utilities.laginnsendingsId(), "Test søknad", "BID", date
+				Utilities.laginnsendingsId(), "Test søknad", "BID", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(7))
 				, listOf(
 					Dokument("NAV 08-09.06","Egenerklæring og sykmelding", "HOVEDDOKUMENT"),
+					Dokument("L7","Kvittering", "VEDLEGG"),
 					Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG")
 				)),
 			ArkiverteSaker(
-				Utilities.laginnsendingsId(), "Ettersending til test søknad", "BID", date
+				Utilities.laginnsendingsId(), "Ettersending til test søknad", "BID", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(2))
 				, listOf(
-					Dokument("NAVe 08-09.06","Egenerklæring og sykmelding", "HOVEDDOKUMENT"),
+					Dokument("NAVe 08-09.06","Ettersending til Egenerklæring og sykmelding", "HOVEDDOKUMENT"),
+					Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG"),
+					Dokument("W2","Et veddlegg", "VEDLEGG"),
+					Dokument("L7","Kvittering", "VEDLEGG")
+				)),
+			ArkiverteSaker(
+				Utilities.laginnsendingsId(), "Test søknad", "PEN", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(14))
+				, listOf(
+					Dokument("NAV 95-00.09","Skjema for bankopplysninger – Australia", "HOVEDDOKUMENT"),
+					Dokument("L7","Kvittering", "VEDLEGG"),
 					Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG")
-				))
+				)),
+		ArkiverteSaker(
+				Utilities.laginnsendingsId(), "Test søknad", "PEN", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(14))
+				, listOf(
+					Dokument("NAV 15-00.04","Søknad om skolepenger – enslig mor eller far", "HOVEDDOKUMENT"),
+					Dokument("L7","Kvittering", "VEDLEGG")
+				)),
+			ArkiverteSaker(
+				Utilities.laginnsendingsId(), "Ettersending til test søknad", "BID", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(5))
+				, listOf(
+					Dokument("NAVe 95-00.09","Ettersending til Skjema for bankopplysninger – Australia", "HOVEDDOKUMENT"),
+					Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG"),
+					Dokument("L7","Kvittering", "VEDLEGG"),
+					Dokument("W2","Et ekstra veddlegg", "VEDLEGG")
+				)),
+			ArkiverteSaker(
+				Utilities.laginnsendingsId(), "Ettersending til test søknad", "BID", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(20))
+				, listOf(
+					Dokument(null,"Ettersending til Skjema for bankopplysninger – Australia", "HOVEDDOKUMENT"),
+					Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG"),
+					Dokument("L7","Kvittering", "VEDLEGG"),
+					Dokument(null,"Et ekstra veddlegg", "VEDLEGG")
+				)),
+			ArkiverteSaker(
+				Utilities.laginnsendingsId(), "Ettersending til test søknad", "BID", DateTimeFormatter.ISO_OFFSET_DATE_TIME.format( OffsetDateTime.now().minusDays(25))
+			, listOf(
+				Dokument("","Ettersending til Skjema for bankopplysninger – Australia", "HOVEDDOKUMENT"),
+				Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG"),
+				Dokument("L7","Kvittering", "VEDLEGG"),
+				Dokument("","Et ekstra veddlegg", "VEDLEGG")
+			))
 		),
 		"12345678902" to listOf(
 			ArkiverteSaker(
 				Utilities.laginnsendingsId(), "Test søknad", "BID", date
 			, listOf(
 				Dokument("NAV 08-09.06","Egenerklæring og sykmelding", "HOVEDDOKUMENT"),
+				Dokument("L7","Kvittering", "VEDLEGG"),
 				Dokument("N6","Et vedleggEgenerklæring og sykmelding", "VEDLEGG")
 			))
 		),
