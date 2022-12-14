3938a67bf345e9ee901a0f0255972cdcd7c28fc2
nilsarne <nils-arne.ringstad@nav.no>
2022-12-14 10:05:27 +0100

    Lagt til logging av hvilke vedlegg som opprettes

--- innsender/src/main/kotlin/no/nav/soknad/innsending/rest/SkjemaRestApi.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/rest/SkjemaRestApi.kt
@@ -34,6 +34,7 @@ class SkjemaRestApi(
 		val brukerId = tilgangskontroll.hentBrukerFraToken()
 		soknadService.sjekkHarAlleredeSoknadUnderArbeid(brukerId, skjemaDto.skjemanr, false)
 		val opprettetSoknadId = soknadService.opprettNySoknad(SkjemaDokumentSoknadTransformer().konverterTilDokumentSoknadDto(skjemaDto, brukerId))
+		logger.debug("opprettetSoknadId: Soknad fra fyllut persistert. Antall vedlegg fra FyllUt=${skjemaDto.vedleggsListe?.size}")
 		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(restConfig.frontEndFortsettEndpoint+opprettetSoknadId)).build()
 	}
 }
