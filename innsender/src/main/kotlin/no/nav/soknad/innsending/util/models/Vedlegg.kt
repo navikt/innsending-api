0542309f8d191fcb9876d141b7adee588966001b
Morten Stulen <morten.elias.stulen@nav.no>
2023-06-30 08:59:15 +0200

    Legg til test

--- innsender/src/main/kotlin/no/nav/soknad/innsending/util/models/Vedlegg.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/util/models/Vedlegg.kt
@@ -1,13 +1,24 @@
 package no.nav.soknad.innsending.util.models
 
+import no.nav.soknad.innsending.model.VedleggDto
 import no.nav.soknad.innsending.repository.VedleggDbData
+
+
+val List<VedleggDbData>.hovedDokument: VedleggDbData?
+	get() {
+		return this.find { it.erhoveddokument && !it.ervariant }
+	}
 
 val List<VedleggDbData>.hovedDokumentVariant: VedleggDbData?
 	get() {
 		return this.find { it.erhoveddokument && it.ervariant }
 	}
 
-val List<VedleggDbData>.hovedDokument: VedleggDbData?
+val List<VedleggDto>.hovedDokument: VedleggDto?
 	get() {
-		return this.find { it.erhoveddokument && !it.ervariant }
+		return this.find { it.erHoveddokument && !it.erVariant }
+	}
+val List<VedleggDto>.hovedDokumentVariant: VedleggDto?
+	get() {
+		return this.find { it.erHoveddokument && it.erVariant }
 	}
