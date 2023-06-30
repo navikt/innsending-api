0be5baf550da7f92a56dc268819c97af40192b50
Morten Stulen <morten.elias.stulen@nav.no>
2023-06-30 13:09:32 +0200

    Formatering

--- innsender/src/test/kotlin/no/nav/soknad/innsending/cleanup/LeaderSelectionTest.kt
+++ innsender/src/test/kotlin/no/nav/soknad/innsending/cleanup/LeaderSelectionTest.kt
@@ -3,7 +3,6 @@ package no.nav.soknad.innsending.cleanup
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import no.nav.soknad.innsending.ApplicationTest
