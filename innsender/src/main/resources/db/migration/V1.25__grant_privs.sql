eced742bb92cd20e8ed1d4a6b80df4b633eaf237
Morten Stulen <morten.elias.stulen@nav.no>
2023-08-09 10:30:38 +0200

    Oppdater sql

--- innsender/src/main/resources/db/migration/V1.25__grant_privs.sql
+++ innsender/src/main/resources/db/migration/V1.25__grant_privs.sql
@@ -1,1 +1,11 @@
-grant all on all tables in schema public to cloudsqliamuser;
+DO
+$$
+	BEGIN
+		IF EXISTS
+				(SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
+		THEN
+			GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+			ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
+		END IF;
+	END
+$$;
