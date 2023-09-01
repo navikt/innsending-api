f7abbdd4e84694000f339c8019d9c54f95904202
Morten Stulen <morten.elias.stulen@nav.no>
2023-09-01 08:51:33 +0200

    Legg til variasjoner av innsendingsId

--- innsender/src/main/kotlin/no/nav/soknad/innsending/interceptor/MdcInterceptor.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/interceptor/MdcInterceptor.kt
@@ -7,18 +7,30 @@ import no.nav.soknad.innsending.util.MDCUtil
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.HandlerMapping
+import java.util.*
 
 @Component
 class MdcInterceptor : HandlerInterceptor {
+	// Legg til innsendingsId i MDC fra header eller pathvariabel
 	override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
 		val pathVariables = request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE) as Map<*, *>
+		val headerNames = Collections.list(request.headerNames)
 
-		request.getHeader("X-innsendingId")?.let {
+		val variations = arrayOf("x-innsendingid", "x-innsendingsid", "innsendingid", "innsendingsid")
+		val innsendingsIdHeader = variations
+			.map { variation -> headerNames.find { it.equals(variation, ignoreCase = true) } }
+			.firstNotNullOfOrNull { variationHeader -> variationHeader?.let { request.getHeader(it) } }
+
+		val innsendingsIdPathVariable = variations
+			.map { variation -> pathVariables[variation] }
+			.firstNotNullOfOrNull { variationValue -> variationValue?.toString() }
+
+		innsendingsIdHeader?.let {
 			MDCUtil.toMDC(MDC_INNSENDINGS_ID, it)
 		}
 
-		pathVariables["innsendingsId"]?.let {
-			MDCUtil.toMDC(MDC_INNSENDINGS_ID, it.toString())
+		innsendingsIdPathVariable?.let {
+			MDCUtil.toMDC(MDC_INNSENDINGS_ID, it)
 		}
 
 		return true
