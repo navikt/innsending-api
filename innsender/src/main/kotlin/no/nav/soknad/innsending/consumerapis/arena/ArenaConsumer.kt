c02a89a0e082e9f97982715d5d1889ad51ab03d0
Morten Stulen <morten.elias.stulen@nav.no>
2023-11-07 11:16:19 +0100

    Use correct dates

--- innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/arena/ArenaConsumer.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/arena/ArenaConsumer.kt
@@ -2,38 +2,45 @@ package no.nav.soknad.innsending.consumerapis.arena
 
 import no.nav.soknad.innsending.config.RestConfig
 import no.nav.soknad.innsending.consumerapis.arena.dto.Maalgruppe
-import no.nav.soknad.innsending.exceptions.*
+import no.nav.soknad.innsending.exceptions.BackendErrorException
 import no.nav.soknad.innsending.security.SubjectHandlerInterface
 import no.nav.soknad.innsending.util.Constants.NAV_PERSON_IDENT
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.awaitBodyOrNull
+import org.springframework.web.util.UriComponentsBuilder
+import java.time.LocalDate
 
 @Component
 @Profile("test | dev | prod")
 class ArenaConsumer(
 	@Qualifier("arenaClient") private val webClient: WebClient,
-	@Value("\$spring.application.name}") private val applicationName: String,
 	private val subjectHandler: SubjectHandlerInterface,
 	private val restConfig: RestConfig,
 ) : ArenaConsumerInterface {
 
 	private val logger: Logger = LoggerFactory.getLogger(javaClass)
+	private val fromDate = LocalDate.now().minusMonths(6)
+	private val toDate = LocalDate.now().plusMonths(2)
 
-	override suspend fun getMaalgruppe(): List<Maalgruppe> {
+	override suspend fun getMaalgrupper(): List<Maalgruppe> {
 		logger.info("Henter målgruppe")
+
+		val uri = UriComponentsBuilder
+			.fromUriString("${restConfig.arenaUrl}/api/v1/maalgrupper")
+			.queryParam("fom", fromDate)
+			.queryParam("tom", toDate)
+			.build()
+			.toUri()
 
 		return webClient
 			.get()
-			.uri("${restConfig.arenaUrl}/api/v1/maalgrupper?fom=2023-01-01") // FIXME: Add correct date
+			.uri(uri)
 			.accept(MediaType.APPLICATION_JSON)
 			.header(NAV_PERSON_IDENT, subjectHandler.getUserIdFromToken())
 			.retrieve()
@@ -43,26 +50,4 @@ class ArenaConsumer(
 			)
 	}
 
-	private fun handleError(error: Throwable, serviceName: String) {
-		if (error is WebClientResponseException) {
-			val statusCode = error.statusCode
-			val responseBody = error.responseBodyAsString
-			val errorMessage =
-				String.format("Kall mot %s feilet (statuskode: %s). Body: %s", serviceName, statusCode, responseBody)
-
-			if (statusCode == HttpStatus.UNAUTHORIZED) {
-				throw ClientErrorUnauthorizedException(errorMessage, error, ErrorCode.ARENA_UNAUTHORIZED)
-			}
-
-			if (statusCode == HttpStatus.FORBIDDEN) {
-				throw ClientErrorForbiddenException(errorMessage, error, ErrorCode.ARENA_FORBIDDEN)
-			}
-
-			if (statusCode.is4xxClientError) {
-				throw IllegalActionException(errorMessage, error, ErrorCode.GENERAL_ERROR)
-			}
-
-			throw BackendErrorException(errorMessage, error, ErrorCode.GENERAL_ERROR)
-		}
-	}
 }
