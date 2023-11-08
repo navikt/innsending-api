3c7139e39baf248a55f89a6c364d4fd2709b7ce2
Morten Stulen <morten.elias.stulen@nav.no>
2023-11-08 12:45:17 +0100

    Fix test

--- innsender/src/test/kotlin/no/nav/soknad/innsending/util/prefill/ServicePropertiesTest.kt
+++ innsender/src/test/kotlin/no/nav/soknad/innsending/util/prefill/ServicePropertiesTest.kt
@@ -1,5 +1,7 @@
 package no.nav.soknad.innsending.util.prefill
 
+import no.nav.soknad.innsending.util.Constants.ARENA_MAALGRUPPER
+import no.nav.soknad.innsending.util.Constants.PDL
 import no.nav.soknad.innsending.util.prefill.ServiceProperties.createServicePropertiesMap
 import no.nav.soknad.innsending.util.prefill.ServiceProperties.findKeyForString
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -11,32 +13,31 @@ class ServicePropertiesTest {
 	fun `Should create correct service properties map based on input properties`() {
 		// Given
 		val inputList = listOf("sokerFornavn", "sokerMaalgrupper")
-		val serviceKeyMap =
-			mapOf("PDL" to listOf("sokerFornavn", "sokerEtternavn"), "ARENA" to listOf("sokerMaalgrupper"))
 
 		// When
 		val result = createServicePropertiesMap(inputList)
 
 		// Then
 		assertEquals(2, result.size)
-		assertTrue(result.containsKey("PDL"))
-		assertTrue(result.containsKey("ARENA"))
-		assertEquals(1, result["PDL"]?.size)
-		assertEquals(1, result["ARENA"]?.size)
-		assertEquals("sokerFornavn", result["PDL"]?.get(0))
-		assertEquals("sokerMaalgrupper", result["ARENA"]?.get(0))
+		assertTrue(result.containsKey(PDL))
+		assertTrue(result.containsKey(ARENA_MAALGRUPPER))
+		assertEquals(1, result[PDL]?.size)
+		assertEquals(1, result[ARENA_MAALGRUPPER]?.size)
+		assertEquals("sokerFornavn", result[PDL]?.get(0))
+		assertEquals("sokerMaalgrupper", result[ARENA_MAALGRUPPER]?.get(0))
 	}
 
 	@Test
 	fun `Should find key in map for input string`() {
 		// Given
 		val inputString = "sokerFornavn"
-		val inputMap = mapOf("PDL" to listOf("sokerFornavn", "sokerEtternavn"), "ARENA" to listOf("sokerMaalgrupper"))
+		val inputMap =
+			mapOf(PDL to listOf("sokerFornavn", "sokerEtternavn"), ARENA_MAALGRUPPER to listOf("sokerMaalgrupper"))
 
 		// When
 		val result = findKeyForString(inputString, inputMap)
 
 		// Then
-		assertEquals("PDL", result)
+		assertEquals(PDL, result)
 	}
 }
