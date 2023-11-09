972b594043ecc17fe4b3d5ea3f6e89e4d85acad1
Morten Stulen <morten.elias.stulen@nav.no>
2023-11-09 16:08:01 +0100

    Remove unused exception

--- innsender/src/main/kotlin/no/nav/soknad/innsending/exceptions/SoknadAlreadyExistsException.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.soknad.innsending.exceptions
-
-class SoknadAlreadyExistsException(
-	override val message: String,
-	override val cause: Throwable? = null,
-	val errorCode: ErrorCode = ErrorCode.SOKNAD_ALREADY_EXISTS
-) : RuntimeException(message)
