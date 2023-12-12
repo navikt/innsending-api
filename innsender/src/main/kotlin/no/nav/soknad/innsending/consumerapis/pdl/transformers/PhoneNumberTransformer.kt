3e3890fe7ea4308f754ddf4312c57755e5793b23
Morten Stulen <morten.elias.stulen@nav.no>
2023-12-12 13:14:44 +0100

    Fix phonenumber with null value

--- innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/pdl/transformers/PhoneNumberTransformer.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/consumerapis/pdl/transformers/PhoneNumberTransformer.kt
@@ -4,11 +4,15 @@ import no.nav.soknad.innsending.pdl.generated.prefilldata.Telefonnummer
 
 
 object PhoneNumberTransformer {
-	fun transformPhoneNumbers(phoneNumbers: List<Telefonnummer>?): String {
+	fun transformPhoneNumbers(phoneNumbers: List<Telefonnummer>?): String? {
 		val prioritizedNumber = phoneNumbers?.minByOrNull { it.prioritet } ?: phoneNumbers?.firstOrNull()
 
 		val trimmedNumber = prioritizedNumber?.nummer?.trim()?.replace(" ", "")
 		val trimmedLandskode = prioritizedNumber?.landskode?.trim()?.replace(" ", "")
+
+		if (trimmedNumber == null || trimmedLandskode == null) {
+			return null
+		}
 
 		return "${trimmedLandskode}${trimmedNumber}"
 	}
