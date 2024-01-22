c0ad92f636fc3c124663e26cdcabbce0edfc39a1
nilsarne <nils-arne.ringstad@nav.no>
2024-01-22 08:51:54 +0100

    Eksperimentering med isolation level i transaction  for å "se" updates gjort i egen transaksjon virket ikke, forsøker å sette flushAutomatically på repo kallet. Satt IsloationLevel.READ_UNCOMMITTED. Endret fra flushAutomatically til clearAutomatically

--- innsender/src/main/kotlin/no/nav/soknad/innsending/repository/VedleggRepository.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/repository/VedleggRepository.kt
@@ -14,6 +14,7 @@ import java.time.LocalDateTime
 interface VedleggRepository : JpaRepository<VedleggDbData, Long> {
 
 	@Query(value = "FROM VedleggDbData WHERE soknadsid = :soknadsid order by id")
+	@Modifying(clearAutomatically = true)
 	fun findAllBySoknadsid(@Param("soknadsid") soknadsid: Long): List<VedleggDbData>
 
 	@Query(value = "FROM VedleggDbData WHERE id = :vedleggsid")
