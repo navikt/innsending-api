c317732474205a3dbc28999a384fe1950569757d
nilsarne <nils-arne.ringstad@nav.no>
2024-04-10 12:50:14 +0200

    Lagt til test av bytte av tema i forbindelse med støtte til flytting

--- innsender/src/test/kotlin/no/nav/soknad/innsending/service/TilleggsstonadServiceTest.kt
+++ innsender/src/test/kotlin/no/nav/soknad/innsending/service/TilleggsstonadServiceTest.kt
@@ -173,6 +173,64 @@ class TilleggsstonadServiceTest: InnsendingServiceTest() {
 
 
 	@Test
+	fun sendInnTilleggssoknad_flytting() {
+		val innsendingService = lagInnsendingService(soknadService)
+		val skjemaNr = "NAV 11-12.23B"
+		val hoveddokDto = Hjelpemetoder.lagVedlegg(
+			vedleggsnr = skjemaNr,
+			tittel = "Tilleggssoknad",
+			erHoveddokument = true,
+			erVariant = false,
+			opplastingsStatus = OpplastingsStatusDto.lastetOpp,
+			vedleggsNavn = "/litenPdf.pdf"
+		)
+		val hoveddokVariantDto = Hjelpemetoder.lagVedlegg(
+			vedleggsnr = skjemaNr,
+			tittel = "Tilleggssoknad",
+			erHoveddokument = true,
+			erVariant = true,
+			opplastingsStatus = OpplastingsStatusDto.lastetOpp,
+			vedleggsNavn = "/__files/flytteutgifter-NAV-11-12.23B-10042024.json"
+		)
+		val inputDokumentSoknadDto = Hjelpemetoder.lagDokumentSoknad(
+			skjemanr = skjemaNr,
+			tittel = "Tilleggssoknad",
+			brukerId = testpersonid,
+			vedleggsListe = listOf(hoveddokDto, hoveddokVariantDto),
+			spraak = "no_NB",
+			tema = "TSO"
+		)
+		val skjemaDto =
+			SoknadAssertions.testOgSjekkOpprettingAvSoknad(soknadService = soknadService, inputDokumentSoknadDto)
+
+		val opprettetSoknad = soknadService.hentSoknad(skjemaDto.innsendingsId!!)
+		val kvitteringsDto =
+			SoknadAssertions.testOgSjekkInnsendingAvSoknad(
+				soknadsmottakerAPI,
+				opprettetSoknad,
+				innsendingService
+			)
+		Assertions.assertTrue(kvitteringsDto.hoveddokumentRef != null)
+		Assertions.assertTrue(kvitteringsDto.innsendteVedlegg!!.isEmpty())
+		Assertions.assertTrue(kvitteringsDto.skalEttersendes!!.isEmpty())
+
+		val innsendtSoknad = soknadService.hentSoknadMedHoveddokumentVariant(opprettetSoknad.innsendingsId!!)
+		Assertions.assertTrue(innsendtSoknad.status == SoknadsStatusDto.innsendt)
+		Assertions.assertEquals("TSR", innsendtSoknad.tema)
+		Assertions.assertEquals(
+			Mimetype.applicationSlashXml,
+			innsendtSoknad.vedleggsListe.filter { it.erHoveddokument && it.erVariant && it.opplastingsStatus == OpplastingsStatusDto.innsendt }
+				.first().mimetype
+		)
+		Assertions.assertEquals(
+			Mimetype.applicationSlashJson,
+			innsendtSoknad.vedleggsListe.filter { it.erHoveddokument && it.erVariant && it.opplastingsStatus == OpplastingsStatusDto.sendesIkke }
+				.first().mimetype
+		)
+
+	}
+
+	@Test
 	fun sendInnKjoreliste() {
 		val innsendingService = lagInnsendingService(soknadService)
 		val hoveddokDto = Hjelpemetoder.lagVedlegg(
