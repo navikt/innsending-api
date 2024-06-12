983f574405b25f78bf052e7003a51078cce448dd
Morten Stulen <morten.elias.stulen@nav.no>
2024-06-12 11:49:42 +0200

    Use 28 days instead of 30

--- innsender/src/main/resources/db/migration/V1.27__add_skalslettesdato_column.sql
+++ innsender/src/main/resources/db/migration/V1.27__add_skalslettesdato_column.sql
@@ -2,5 +2,5 @@ ALTER TABLE soknad
 ADD COLUMN skalslettesdato TIMESTAMP WITH TIME ZONE;
 
 UPDATE soknad
-SET skalslettesdato = opprettetdato + INTERVAL '30 days'
+SET skalslettesdato = opprettetdato + INTERVAL '28 days'
 WHERE skalslettesdato IS NULL;
