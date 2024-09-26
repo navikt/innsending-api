6f7193b6da58328186228c9f35a1a2471751d190
nilsarne <nils-arne.ringstad@nav.no>
2024-09-26 15:37:24 +0200

    Endring av forside for å følge brevmal oppsett for NAV ikon og plassering og størrelse på tekstkomponenter

--- innsender/src/main/kotlin/no/nav/soknad/innsending/rest/ekstern/EksternRestApi.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/rest/ekstern/EksternRestApi.kt
@@ -57,7 +57,7 @@ class EksternRestApi(
 		)
 
 		combinedLogger.log(
-			"[${applikasjon}] - ${ettersending.innsendingsId}: Opprettet ettersending fra ekstern applikasjon på skjema ${ettersending.skjemanr}",
+			"[${applikasjon}] - ${ettersending.innsendingsId}: **Opprettet ettersending fra ekstern applikasjon på skjema ${ettersending.skjemanr}",
 			brukerId
 		)
 
@@ -106,7 +106,12 @@ class EksternRestApi(
 		}
 
 		val typeFilter = soknadstyper?.toTypedArray() ?: emptyArray()
-		brukerIds.forEach { combinedLogger.log("[${applikasjon}] - Henter søknader med søknadstyper=${soknadstyper ?: "<alle>"} for $skjemanr", it) }
+		brukerIds.forEach {
+			combinedLogger.log(
+				"[${applikasjon}] - Henter søknader med søknadstyper=${soknadstyper ?: "<alle>"} for $skjemanr",
+				it
+			)
+		}
 		val soknader = brukerIds.flatMap { soknadService.hentAktiveSoknader(it, skjemanr, *typeFilter) }
 
 		return ResponseEntity.status(HttpStatus.OK).body(soknader)
