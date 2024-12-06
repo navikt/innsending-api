d2442a749642604863d3eda14f394fe96930be19
nilsarne <nils-arne.ringstad@nav.no>
2024-12-06 08:56:41 +0100

    Fiksing av tester og lagt til manglende url for load test

--- innsender/src/main/kotlin/no/nav/soknad/pdfutilities/gotenberg/ToPdfConverterTest.kt
+++ innsender/src/main/kotlin/no/nav/soknad/pdfutilities/gotenberg/ToPdfConverterTest.kt
@@ -2,9 +2,16 @@ package no.nav.soknad.pdfutilities.gotenberg
 
 import no.nav.soknad.pdfutilities.FileToPdfInterface
 import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.pdmodel.common.PDStream
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.font.PDType1Font
+import org.apache.pdfbox.pdmodel.font.PDFont
+import org.apache.pdfbox.pdmodel.font.PDSimpleFont
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
 @Component
@@ -12,14 +19,7 @@ import org.springframework.stereotype.Component
 class ToPdfConverterTest : FileToPdfInterface {
 
 	override fun toPdf(fileName: String, fileContent: ByteArray): ByteArray {
-		var document: PDDocument? = null
-		try {
-			document = PDDocument()
-			val pdStream = PDStream(document)
-			return pdStream.toByteArray()
-		} finally {
-			if (document != null) document.close()
-		}
+		return dummyPdf()
 	}
 
 	override fun imageToPdf(fileName: String, fileContent: ByteArray): ByteArray {
@@ -30,4 +30,26 @@ class ToPdfConverterTest : FileToPdfInterface {
 		return toPdf(fileName, docs.first())
 	}
 
+	private fun dummyPdf(): ByteArray {
+		val document = PDDocument()
+
+		try {
+			val page = PDPage()
+			document.addPage(page)
+			PDPageContentStream(document, page).use { contentStream ->
+				contentStream.beginText()
+				contentStream.setFont(PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD), 12f)
+				contentStream.newLineAtOffset(100f, 700f)
+				contentStream.showText("Hello World")
+				contentStream.endText()
+			}
+
+			val outputStream = ByteArrayOutputStream()
+			document.save(outputStream)
+
+			return outputStream.toByteArray()
+		} finally {
+			document.close()
+		}
+	}
 }
