b47b084aa678cd87124eafdb9b80b8c2ed83c416
nilsarne <nils-arne.ringstad@nav.no>
2025-04-06 12:59:09 +0200

    Fikset feil som førte til at det blir opprettet utkast istedenfor oppgave. Lagt til skript som sletter utkast og publiserer oppgave

--- innsender/src/main/kotlin/no/nav/soknad/innsending/supervision/ChaangeNotificationType.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/supervision/ChaangeNotificationType.kt
@@ -28,14 +28,17 @@ class ChaangeNotificationType(
 
 	@Scheduled(cron = "\${cron.runOnceAtSpecificTime}")
 	fun findAllByApplikasjonAndChangeNotificationType() {
+		logger.info("Skal finne og avslutte notifikasjoner og opprette nye")
 		if (leaderSelectionUtility.isLeader()) {
+			logger.info("Er leader")
 
 			val fraTid = LocalDateTime.parse("2025-03-31T08:30:00")
 			val tilTid = LocalDateTime.parse("2025-04-07T09:30:00")
 			val findByApplicationType = repo.hentHendelseGittApplikasjon(bidragApplikasjon, HendelseType.Opprettet)
 				.filter { it.tidspunkt.isAfter(fraTid) && it.tidspunkt.isBefore(tilTid) }
-				.filter { it.innsendingsid == "301a3e73-c381-4784-bd20-28ea06cab13e" } // TODO remove
+				.filter { it.innsendingsid == "301a3e73-c381-4784-bd20-28ea06cab13e" || it.innsendingsid == "74aaa380-dcfd-41c2-bd81-8f53b7e8ee7e" || it.innsendingsid == "73a03e46-98b9-417d-a29c-e8e7a9a7f5de" } // TODO remove
 
+			logger.info("Antall søknder funnet som kanskje har fått feil notifikasjonstype")
 			findByApplicationType.forEach { soknad -> findApplicationCloseAndOpenNotification(soknad.innsendingsid) }
 		}
 	}
@@ -43,6 +46,7 @@ class ChaangeNotificationType(
 	fun findApplicationCloseAndOpenNotification(innsendingId: String) {
 		try {
 			val soknad = repo.hentSoknadDb(innsendingId)
+			logger.info("$innsendingId: funnet ? ${soknad != null}, status = ${soknad.status} Skal avslutte notifikasjon og opprette ny")
 			if (soknad != null || soknad.status != SoknadsStatus.Opprettet) return
 
 			logger.info("$innsendingId: Skal avslutte notifikasjon og opprette ny")
