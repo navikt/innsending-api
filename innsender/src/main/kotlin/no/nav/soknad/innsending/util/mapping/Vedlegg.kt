086b7072cf8ba71ff98d440b882dcde96b0dc125
nilsarne <nils-arne.ringstad@nav.no>
2025-08-26 09:52:37 +0200

    Oppdatert SkjemaDtoV2 sin SkjemaDokumentDtoV2 med opsjonell filidListe. Lagt til logging for debugging

--- innsender/src/main/kotlin/no/nav/soknad/innsending/util/mapping/Vedlegg.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/util/mapping/Vedlegg.kt
@@ -22,7 +22,7 @@ fun mapTilVedleggDb(
 	VedleggDbData(
 		id = vedleggsId ?: vedleggDto.id,
 		soknadsid = soknadsId,
-		status = opplastingsStatus,
+		status = mapTilDbOpplastingsStatus(vedleggDto.opplastingsStatus),
 		erhoveddokument = vedleggDto.erHoveddokument,
 		ervariant = vedleggDto.erVariant,
 		erpdfa = vedleggDto.erPdfa,
