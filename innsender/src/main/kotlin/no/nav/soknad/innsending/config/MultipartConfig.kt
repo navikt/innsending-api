08126d002fe6d37a2ac61c8467fdc69220fec1e3
nilsarne <nils-arne.ringstad@nav.no>
2025-10-21 16:02:54 +0200

    Økt maks størrelse på vedlegg fra 50Mb til 150Mb og maks størrelse på søknad fra 150Mb til 300Mb

--- innsender/src/main/kotlin/no/nav/soknad/innsending/config/MultipartConfig.kt
+++ innsender/src/main/kotlin/no/nav/soknad/innsending/config/MultipartConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.soknad.innsending.config
 
 import jakarta.servlet.MultipartConfigElement
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.MultipartConfigFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,11 +11,17 @@ import org.springframework.util.unit.DataSize
 @Configuration
 class MultipartConfig {
 
+	@Value("\${multipart-file-size}")
+	private val maxMultipartFileSize: Long = 151
+
+	@Value("\${multipart-request-size}")
+	private val maxMultipartRequestSize: Long = 301
+
 	@Bean
 	fun multipartConfigElement(): MultipartConfigElement? {
 		val factory = MultipartConfigFactory()
-		factory.setMaxFileSize(DataSize.ofMegabytes(51))
-		factory.setMaxRequestSize(DataSize.ofMegabytes(100))
+		factory.setMaxFileSize(DataSize.ofMegabytes(maxMultipartFileSize))
+		factory.setMaxRequestSize(DataSize.ofMegabytes(maxMultipartRequestSize))
 		return factory.createMultipartConfig()
 	}
 }
