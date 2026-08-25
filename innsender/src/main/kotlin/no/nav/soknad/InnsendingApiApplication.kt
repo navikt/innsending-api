ac766840d679b3dc6f2abb5d1f841c4a48b322bf
nilsarne <nils-arne.ringstad@nav.no>
2026-08-25 10:50:08 +0200

    Oppdatering etter PR. Etter avklaring med chatgpt har jeg fjernet exclusion av SecurityAutoConfiguration

--- innsender/src/main/kotlin/no/nav/soknad/InnsendingApiApplication.kt
+++ innsender/src/main/kotlin/no/nav/soknad/InnsendingApiApplication.kt
@@ -3,12 +3,11 @@ package no.nav.soknad
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
 import org.springframework.resilience.annotation.EnableResilientMethods
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
-@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+@SpringBootApplication()
 @EnableTransactionManagement
 @ConfigurationPropertiesScan
 @EnableResilientMethods
